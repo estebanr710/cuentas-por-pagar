f9d90928be83961a7551e0860aa048ff58bf626f
estebanr710 <jerp152@gmail.com>
2023-10-11 19:36:07 -0500

    ...

--- src/infrastructure/repositories/mysql.repository.ts
+++ src/infrastructure/repositories/mysql.repository.ts
@@ -1,5 +1,17 @@
+import { UserEntity } from "../../domain/user/user.entity";
 import { UserRepository } from "../../domain/user/user.repository";
 
-/* export class SqlRepository implements UserRepository {
+export class MysqlRepository implements UserRepository {
+
+    findUserById(uuid: string): Promise<UserEntity | null> {
+        throw new Error(`...`);
+    }
     
-} */
+    registerUser(user: UserEntity): Promise<UserEntity | null> {
+        throw new Error(`...`);
+    }
+    
+    listUser(): Promise<UserEntity[] | null> {
+        throw new Error(`...`);
+    }
+}
