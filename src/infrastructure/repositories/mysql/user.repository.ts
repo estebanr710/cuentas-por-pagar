8ec7cb233acaabbf6d132b024d08fe6fdf5f795e
Esteban <jerp152@gmail.com>
2023-10-20 15:27:24 -0500

    ...

--- src/infrastructure/repositories/mysql/user.repository.ts
+++ src/infrastructure/repositories/mysql/user.repository.ts
@@ -6,7 +6,7 @@ export class MySqlUserRepository implements UserRepository {
 
     constructor(private mysqlRoleRepository = new MySqlRoleRepository) {}
 
-    async updateUser({ id, role_id }: { id: string, role_id: string }): Promise<any | null> {
+    async updateUser({ id, role_id }: { id: string, role_id: string }): Promise<string> {
         let msg = '';
         const CHECK_ROLE = await this.mysqlRoleRepository.listRole(role_id);
         if (CHECK_ROLE) {  
