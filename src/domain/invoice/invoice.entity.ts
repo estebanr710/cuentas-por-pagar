1b855da6116d0ff1d82ed380c1cd7929554480b5
Esteban <jerp152@gmail.com>
2023-11-02 16:03:25 -0500

    ...

--- src/domain/invoice/invoice.entity.ts
+++ src/domain/invoice/invoice.entity.ts
@@ -2,13 +2,13 @@ export interface InvoiceEntity {
     id?: string,
     inv_reference?: string | undefined,
     inv_title: string | null,
-    provider_id: string | null,
+    provider_id?: string | null,
     state_id: string,
-    inv_cp_simi: string | null,
+    inv_cp_simi?: string | null,
     inv_simi_state: boolean,
     inv_email_body: string,
     inv_senders_email: string,
-    inv_amount: number | null,
+    inv_amount?: number | null,
     inv_created_at?: Date,
     inv_modified_at: Date | null | undefined;
     inv_modified_by: string | null | undefined;
