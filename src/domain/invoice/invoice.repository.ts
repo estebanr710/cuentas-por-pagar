56eeb229301f86c69108bd1bd0637325c1b0508d
Esteban <jerp152@gmail.com>
2023-11-02 16:21:10 -0500

    ...

--- src/domain/invoice/invoice.repository.ts
+++ src/domain/invoice/invoice.repository.ts
@@ -4,4 +4,5 @@ export interface InvoiceRepository {
     findInvoiceById(id: string): Promise<InvoiceEntity | null>;
     registerInvoice(invoice: InvoiceEntity): Promise<InvoiceEntity | null>;
     listInvoices(): Promise<InvoiceEntity[] | null>;
+    updateInvoice(invoice: InvoiceEntity): Promise<string | null>;
 }
