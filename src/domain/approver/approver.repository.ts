0824787bc03729553f7afe637ad2bf7586627b89
Esteban <jerp152@gmail.com>
2023-11-08 11:07:21 -0500

    ...

--- src/domain/approver/approver.repository.ts
+++ src/domain/approver/approver.repository.ts
@@ -4,5 +4,5 @@ export interface ApproverRepository {
     registerApprover(approver: ApproverEntity): Promise<ApproverEntity | null | string>;
     updateApprover(approver: ApproverEntity): Promise<null | string>;
     getByInvoice(id: string): Promise<any>;
-    /* unlinkApprover(id: string): Promise<null | string>; */
+    /* deleteApprover(id: string): Promise<null | string>; */
 }
