ba8be1da7f4245938a00d6540b724438cdca6d6c
Esteban <jerp152@gmail.com>
2023-11-16 11:02:03 -0500

    ...

--- src/infrastructure/validators/invoice.validator.ts
+++ src/infrastructure/validators/invoice.validator.ts
@@ -4,7 +4,7 @@ import { Request, Response, NextFunction } from "express";
 import validateResults from "../handlers/handle.validator";
 
 const validatorCreateInvoice = [
-    check("inv_title", "Invalid inv_title [min_length: 5, max_length: 200]").exists().notEmpty().isLength({ min: 5, max: 200 }),
+    check("inv_title", "Invalid inv_title [min_length: 1, max_length: 200]").exists().notEmpty().isLength({ min: 1, max: 200 }),
     check("inv_email_body", "Invalid inv_email_body").exists().notEmpty(),
     check("inv_senders_email", "Invalid inv_senders_email [min_length: 5, max_length: 300]").exists().notEmpty().isLength({ min: 5, max: 100 }),
     (req: Request, res: Response, next: NextFunction) => {
