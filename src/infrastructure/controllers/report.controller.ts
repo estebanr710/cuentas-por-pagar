e1f3376d22c80b3172c5b1f0bdd8e662f351ca9d
Esteban <jerp152@gmail.com>
2023-11-17 10:54:35 -0500

    ...

--- src/infrastructure/controllers/report.controller.ts
+++ src/infrastructure/controllers/report.controller.ts
@@ -3,30 +3,67 @@ import * as fs from "fs";
 import { Request, Response } from "express";
 import { matchedData } from "express-validator";
 
+import { LOCAL_SEQUELIZE } from "../db/mysql/local";
+import randomString from "../handlers/handle.random.string";
+
 export class ReportController {
 
     public generateController = async (req: Request, res: Response) => {
         try {
             let { simi_state, state, from, to } = matchedData(req);
 
+            let query = "";
+
+            // Set query conditions
+            
+            if (simi_state || state || from || to) {
+                query += " WHERE";
+            }
+            
+            // ./Set query conditions
+            
+            const [ DATA, META_DATA ] = await LOCAL_SEQUELIZE.query(query);
+            
+            if (DATA.length === 0) {
+                res.status(404).send("NO_MATCHES_FOUND");
+            }
+            
             const workbook = new excel.Workbook();
             const worksheet = workbook.addWorksheet('Informe');
 
-            worksheet.addRow(['Nombre', 'Edad']);
-            worksheet.addRow(['Juan', 25]);
-            worksheet.addRow(['María', 30]);
-            worksheet.addRow(['Pedro', 22]);
+            // Headers
+            worksheet.addRow([
+                "REFERENCIA",
+                "TÍTULO",
+                "PROVEEDOR",
+                "ESTADO",
+                "CP SIMI",
+                "CONTABILIZADO",
+                "VALOR",
+                "CREADA",
+                "MODIFICADA",
+                "MODIFICADA POR",
+                "ADMINISTRADA",
+                "ADMINISTRADA POR"
+            ]);
+
+            for (const e of DATA) {
+                /* worksheet.addRow([
+
+                ]); */
+            }
 
             const TEMP_PATH = `${__dirname}/../temp`;
+            const TEMP_NAME = `${randomString()}.xlsx`;
+            const FILE_PATH = `${TEMP_PATH}/${TEMP_NAME}`;
 
-            const filePath = `${TEMP_PATH}/ejemplo.xlsx`;
-            await workbook.xlsx.writeFile(filePath);
+            await workbook.xlsx.writeFile(FILE_PATH);
 
-            res.download(`${TEMP_PATH}/ejemplo.xlsx`, (err) => {
+            res.download(FILE_PATH, (err) => {
                 if (err) {
                     res.status(500).send(`Error: ${err}`);
                 } else {
-                    fs.unlinkSync(`${TEMP_PATH}/ejemplo.xlsx`);
+                    fs.unlinkSync(FILE_PATH);
                 };
             });
         } catch (e) {
